ed466f8418fd0389639ed35fa535ddd475e521ea
marcin <marcin@parabolalab.com>
2017-10-07 19:24:44 +0200

    appvar

---
+++ DataFixtures/AppVar.php
@@ -0,0 +1,61 @@
+<?php 
+
+namespace Parabol\BaseBundle\DataFixtures\AppVar;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Parabol\AdminCoreBundle\Entity\AppVar;
+
+class AppVar implements FixtureInterface
+{
+	protected $manager;
+    protected $namespace = null;
+    protected $namespaceLabels = [];
+
+	public function load(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    public function addNamespace($name, $namespaceLabels)
+    {
+        $this->namespace = $name;
+        $this->namespaceLabels = $namespaceLabels;
+        return $this;
+    }
+
+    public function addAppVar($app, $propertyName, $translations, $params = [])
+    {
+
+        $params = array_merge(['required' => false, 'grid' => 6, 'i18n' => false, 'readonly' => false, 'varType' => 'string', 'cssClass' => null, 'twigAlias' => null], $params);
+        $appVar = new AppVar();
+        $appVar->setNamespace($this->namespace);
+        $appVar->setApp($app);
+        $appVar->setPropertyName($propertyName);
+
+        foreach($translations as $lang => $values)
+        {
+             $appVar->translate($lang)->setNamespaceLabel($this->namespaceLabels[$lang]);
+             foreach($values as $key => $value)
+             {
+                    $method = 'set'.ucfirst($key);
+                    $appVar->translate($lang)->$method($value); 
+             }            
+        }
+
+        $appVar->setCssClass($params['cssClass']);
+        $appVar->setVarType($params['varType']);
+        $appVar->setI18n($params['i18n']);
+        $appVar->setGrid($params['grid']);    
+        $appVar->setIsRequired($params['required']);    
+        $appVar->setIsReadonly((boolean)$params['readonly']);
+        $appVar->setTwigAlias($params['twigAlias']); 
+
+        
+        $appVar->mergeNewTranslations();
+
+        $this->manager->persist($appVar);
+
+        return $this;
+    }
+}
